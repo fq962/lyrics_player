3cf9532c276a52388d8f39ec24f5e455c58e7af6
haide <df.quintanillah@gmail.com>
2025-07-17 11:10:00 -0600

    Actualiza la configuración de Supabase en "Lyrics Player" para utilizar variables de entorno en lugar de valores codificados. Esto mejora la seguridad y flexibilidad de la aplicación.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://zxdzhdajrhoxwqkwhsqv.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4ZHpoZGFqcmhveHdxa3doc3F2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzOTA0ODYsImV4cCI6MjA2Nzk2NjQ4Nn0.limFVK24l9CGDtu-cQGAiWVmq4gM5uikXhQVlvRzEjA";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
